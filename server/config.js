3ed1053c789f340a3ff31368eef1ff71f1f34bda
Swarnendu Bhandari <94161758+Swarnendu0123@users.noreply.github.com>
2024-02-01 15:43:23 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = 'https://request-alalyser-b6o4-jh2mk2z39-swarnendus-projects.vercel.app';
+const BACKEND_URL = 'https://request-alalyser-b6o4.vercel.app';
 
 module.exports = {
     BACKEND_URL
