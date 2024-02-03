fee1e14703f56448ba05f130b37dc7f438c65bc3
Swarnendu Bhandari <94161758+Swarnendu0123@users.noreply.github.com>
2024-02-03 19:04:15 +0530

    name changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div className="text-white bg-black">
-      <h1 className='mb-5 text-4xl font-bold'>Request Analyser </h1>
+      <h1 className='mb-5 text-4xl font-bold'>HTTP Request Inspector </h1>
       <UrlGnerator />
       <HomeRoute />
     </div>
