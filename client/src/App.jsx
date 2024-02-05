c1d334dfcc846bba2e097a4d6b44e1c0191b4f9d
Dharsan15 <dharsanravi6677@gmail.com>
2024-02-05 14:46:02 +0530

    added github icon to the page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,16 @@
-import './App.css'
-import HomeRoute from './components/Home.component'
-import UrlGnerator from './components/UrlGnerator.component'
+import "./App.css";
+import HomeRoute from "./components/Home.component";
+import UrlGnerator from "./components/UrlGnerator.component";
+import Navbar from "./components/Navbar.component";
 
 function App() {
-
   return (
     <div className="text-white bg-black">
-      <h1 className='mb-5 text-4xl font-bold'>HTTP Request Inspector </h1>
+      <Navbar />
       <UrlGnerator />
       <HomeRoute />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
