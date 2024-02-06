1d16034e42e6193b5416fbff1f75330dca7594ae
Swarnendu Bhandari <94161758+Swarnendu0123@users.noreply.github.com>
2024-02-06 18:30:06 +0530

    Stop tracking config.js

--- server/config.js
+++
@@ -1,7 +0,0 @@
-const BACKEND_URL = 'https://request-alalyser-b6o4.vercel.app';
-const FRONTEND_URL = 'https://request-alalyser.vercel.app/';
-
-module.exports = {
-    BACKEND_URL,
-    FRONTEND_URL,
-};
