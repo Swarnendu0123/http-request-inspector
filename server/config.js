13a1bf707eea8b6c6839c7aaa489997e46d0611e
Swarnendu Bhandari <94161758+Swarnendu0123@users.noreply.github.com>
2024-02-06 18:46:09 +0530

    Create config.js

---
+++ server/config.js
@@ -0,0 +1,7 @@
+const BACKEND_URL = 'https://request-alalyser-b6o4.vercel.app';
+const FRONTEND_URL = 'https://request-alalyser.vercel.app/';
+
+module.exports = {
+    BACKEND_URL,
+    FRONTEND_URL,
+};
