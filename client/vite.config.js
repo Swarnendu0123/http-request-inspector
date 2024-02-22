5657d3a522dd68129a25d63ee492fa3bf4534c3e
Swarnendu Bhandari <94161758+Swarnendu0123@users.noreply.github.com>
2024-02-22 23:40:55 +0530

    vite congig done

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import envCompatible from 'vite-plugin-env-compatible'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), envCompatible()],
+  plugins: [react()],
 })
