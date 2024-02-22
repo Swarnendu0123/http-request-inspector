f627236418a0eb725a914a12ce7ba0b8636a474d
Swarnendu Bhandari <94161758+Swarnendu0123@users.noreply.github.com>
2024-02-22 22:23:32 +0530

    done

--- client/src/components/auth/firebase.config.js
+++ client/src/components/auth/firebase.config.js
@@ -14,6 +14,8 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
+console.log(firebaseConfig);
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
