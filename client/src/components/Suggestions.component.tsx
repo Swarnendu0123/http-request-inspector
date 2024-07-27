8b86834ec5c3beb15f619cca54c6ad351eb9a471
Rahul <chattychidiya@gmail.com>
2024-07-27 19:05:21 +0530

    feat: Add suggestions

--- client/src/components/Suggestions.component.tsx
+++ client/src/components/Suggestions.component.tsx
@@ -1,14 +1,46 @@
-import React from "react";
+import { useRecoilValue } from "recoil";
+import selectedMessege from "../store/atoms/selectedReq.atom";
+import { BACKEND_URL } from "../../config";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Markdown from "../../node_modules/react-markdown/index";
 
 const Suggestions = () => {
-    return (
-        <div className="hide-scroll grid gap-4  max-h-[70vh] overflow-y-auto border border-gray-700 bg-gray-800 text-white">
-            <h1 className="font-semibold">Suggestions</h1>
-            <div className="w-[15rem] h-[30vh]">
+	const messege = useRecoilValue(selectedMessege);
+	const [suggestion, setSuggestion] = useState([]);
+	const [loading, setLoading] = useState(false);
 
-            </div>
-        </div>
-    )
-}
+	useEffect(() => {
+		fetchSuggestions();
+	}, [messege]);
+
+	const fetchSuggestions = async () => {
+		setLoading(true);
+		const res = await axios.post(BACKEND_URL + "/generate/enhance", {
+			method: "POST",
+			body: { messege },
+		});
+		console.log(res);
+
+		const suggestion = res.data.data.split("<br>");
+		setSuggestion(suggestion);
+		setLoading(false);
+	};
+
+	return (
+		<div className="hide-scroll w-full grid justify-center gap-4 overflow-y-auto border h-full border-gray-700 bg-gray-800 text-white">
+			<h1 className="font-semibold">Suggestions</h1>
+			{loading ? (
+				<div className="w-[15rem]">Loading...</div>
+			) : (
+				<div className="text-start">
+					{suggestion.map((item, index) => {
+						return <Markdown className={""} key={index}>{item}</Markdown>;
+					})}
+				</div>
+			)}
+		</div>
+	);
+};
 
 export default Suggestions;
