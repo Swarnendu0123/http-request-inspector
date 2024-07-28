674fbeed5627b03abe1f795dbe838e48adc25dc4
Swarnendu <clickswarnendu123@gmail.com>
2024-07-28 21:52:08 +0530

    config changes

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,3 +1,3 @@
 // export const BACKEND_URL: string = 'https://request-alalyser-b6o4.vercel.app';
 
-export const BACKEND_URL: string = "http://localhost:8000";
+export const BACKEND_URL: string = import.meta.env.VITE_BACKEND_URL;
