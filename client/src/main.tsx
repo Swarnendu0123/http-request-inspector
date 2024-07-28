29cc01c930e85f6cecc2fac1edb90fde58c613af
Swarnendu <clickswarnendu123@gmail.com>
2024-07-29 02:40:29 +0530

    removed strict mode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
@@ -6,11 +5,10 @@ import { NextUIProvider } from "@nextui-org/react";
 import { RecoilRoot } from 'recoil';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  
     <NextUIProvider>
     <RecoilRoot>
       <App/>
     </RecoilRoot>
-    </NextUIProvider>
-  </React.StrictMode>,
+    </NextUIProvider>,
 )
