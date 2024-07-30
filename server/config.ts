aab15d9d34f654ef3956748df6b3290d3294e8e6
Swarnendu <clickswarnendu123@gmail.com>
2024-07-30 21:59:51 +0530

    config shifted to env

--- server/config.ts
+++ server/config.ts
@@ -1,7 +1,10 @@
-// const BACKEND_URL = "http://localhost:8000";
-// const FRONTEND_URL = "http://localhost:5173";
-const BACKEND_URL = 'https://request-alalyser-b6o4.vercel.app';
-const FRONTEND_URL = 'https://http-request-inspector-virid.vercel.app';
+const BACKEND_URL = process.env.BACKEND_URL;
+const FRONTEND_URL = process.env.FRONTEND_URL;
+
+
+
+// const BACKEND_URL = 'https://request-alalyser-b6o4.vercel.app';
+// const FRONTEND_URL = 'https://http-request-inspector-virid.vercel.app';
 
 const GEMINI_API = process.env.GEMINI_API_KEY;
 
